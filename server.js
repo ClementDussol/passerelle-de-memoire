e3547987f73048d939370b657bbceb3368f3d6e9
clement dussol <clement.dussol@gmail.com>
2017-03-24 10:25:18 +0100

    requêtes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express  = require('express');
 let mongoose = require('mongoose');
 
+let Resource = require('./src/model/Resource');
+let Tag = require('./src/model/Tag');
 let app = express();
 
 mongoose.connect('mongodb://user:user@ds137530.mlab.com:37530/passerelle_de_memoire');
@@ -9,10 +11,33 @@ app.listen(8080);
 app.use('/', express.static('/dist'));
 
 app.get('/', (req, res) => {
-	res.sendFile('/index.html');
+	res.sendFile(__dirname + '/dist/index.html');
 })
 
 app.get('/img/:name', (req, res) => {
 	let fileName = req.params.name
 	res.sendFile(__dirname + '/dist/resources/' + fileName);
+})
+
+app.get('/api/resources/:tag', (req, res) => {
+	let tag = req.params.tag
+	console.log(typeof tag, tag);
+	
+	Resource.find({}).where('tags').in([tag]).exec((err, data)=>{
+		res.send(data);
+	})
+})
+
+app.get('/api/resources', (req, res) => {
+	
+	Resource.find({}).exec((err, data)=>{
+		res.send(data);
+	})
+})
+
+app.get('/api/tags', (req, res) => {
+	
+	Tag.find({}).select('name').exec((err, data)=>{
+		res.send(data);
+	})
 })
