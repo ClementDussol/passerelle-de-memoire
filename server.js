db9ad20afa848844237a04af04ab1a6dddaeae0b
clement dussol <clement.dussol@gmail.com>
2017-03-24 10:46:42 +0100

    correction des routes

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ let app = express();
 mongoose.connect('mongodb://user:user@ds137530.mlab.com:37530/passerelle_de_memoire');
 
 app.listen(8080);
-app.use('/', express.static('/dist'));
+app.use('/', express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/dist/index.html');
+	res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/img/:name', (req, res) => {
 	let fileName = req.params.name
-	res.sendFile(__dirname + '/dist/resources/' + fileName);
+	res.sendFile('/resources/' + fileName);
 })
 
 app.get('/api/resources/:tag', (req, res) => {
